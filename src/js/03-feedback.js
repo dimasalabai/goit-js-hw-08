df6cdf1587d467cd73c30c67712435095caa1614
Dima <dimasalabay0711@gmail.com>
2023-11-14 21:56:56 +0200

    task-03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form.feedback-form');
+const formEmail = document.querySelector('form input');
+const formMessage = document.querySelector('form textarea');
+
+fillForm();
+
+let formData = {
+  email: '',
+  message: '',
+};
+form.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: formEmail.value,
+      message: formMessage.value,
+    })
+  );
+}
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (formEmail.value && formMessage.value) {
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(formData);
+    formData.email = '';
+    formData.message = '';
+  } else {
+    alert('Увага! Всі поля форми мають бути заповнені!');
+  }
+}
+
+function fillForm() {
+  const savedFormData = localStorage.getItem('feedback-form-state');
+
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    formEmail.value = parsedFormData.email;
+    formMessage.value = parsedFormData.message;
+  } else {
+    formEmail.value = '';
+    formMessage.value = '';
+  }
+}
