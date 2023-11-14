662b8a88f23dde5d1d2206edfb6d9f5b3718538e
Dima <dimasalabay0711@gmail.com>
2023-11-14 21:33:07 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const startPause = localStorage.getItem('videoplayer-current-time');
+
+const onTimeUpdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+if (startPause) {
+  player
+    .setCurrentTime(startPause)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
